1b7c0f80807e9de934f671649604dae58f42eca7
tongmeanfc@gmail.com <76610760+Tongmean@users.noreply.github.com>
2022-09-22 20:24:21 +0700

    D

--- Test/settings.py
+++ Test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3700+p!58jtxnmc#da=-yfz466ulmt!ygr(*(xvjyozi+&dl_8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
